21e11ead240e6816bd60141d8b220673a83d8421
yapjiacong <85666362+yapjiacong@users.noreply.github.com>
2023-11-09 17:13:23 +0800

    w3

---
+++ w4/function.py
@@ -0,0 +1,16 @@
+import os
+os.system('cls')
+
+def areaofcircle(R):
+    c=(22/7)*R**2
+    return c
+    
+a = 7
+
+d = areaofcircle(a)
+print(d)
+
+b = 21
+e = areaofcircle(b)
+print(e)
+
---
+++ w4/function2.py
@@ -0,0 +1,6 @@
+def areacircle(R):
+    c = (22/7)*R**2
+    return c
+b = [3,5,6,7]
+for num in b:
+    print(areacircle(num))
