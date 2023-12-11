d559c6634fee74b2b138ab5eabfcdadea411c547
yapjiacong <85666362+yapjiacong@users.noreply.github.com>
2023-12-11 15:27:53 +0800

    w7

---
+++ w7/Matplotlib_Example_3.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+# Data to plot
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+# Create the plot
+plt.scatter(x, y)
+# Show the plot
+plt.show()
---
+++ w7/Matplotlib_Example_6.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# Generate random data
+num_points = 50
+x = np.random.uniform(0, 10, num_points)
+y = 3 * x + np.random.normal(0, 2, 
+num_points)
+# Fit a linear model
+coeffs = np.polyfit(x, y, 1)
+poly_model = np.poly1d(coeffs)
+# Create the scatter plot
+plt.scatter(x, y, s=50)
+# Plot the regression line
+x_line = np.linspace(x.min(), x.max(), 100)
+y_line = poly_model(x_line)
+plt.plot(x_line, y_line, 'r--')
+# Show the plot
+plt.show()
